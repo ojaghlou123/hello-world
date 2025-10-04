23ca106e535bd7a2f4c6ba843586a2c51a8141dc
ojaghlou123 <ojaghloughoochali@gmail.com>
2025-10-04 11:03:23 +0330

    Draw static Hello World using turtle.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import turtle
+import colorsys
+import time
+
+turtle_screen = turtle.Screen()
+turtle_screen.title("Hello World Static")
+turtle_writer = turtle.Turtle()
+turtle_writer.hideturtle()
+turtle_writer.penup()
+turtle_writer.goto(0,0)
+turtle_writer.write("Hello World", align="center", font=("Arial", 24, "normal"))
+time.sleep(2)  # Pause to view
+turtle_writer.clear()
